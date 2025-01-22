fbf5dac99f629a6dfb2c9fc3c6117bdb24ee4784
Auke van Oostenbrugge <auke.v.o@hotmail.com>
2025-01-22 08:23:45 +0000

    Added test-suite for tap()

--- src/mock/dom.handler.mock.ts
+++
@@ -1,9 +0,0 @@
-import { ImportMap } from "@softarc/native-federation-runtime";
-import { appendImportMapToDOM } from "../lib/utils/dom";
-
-const domHandlerMock = (): DomHandler => {
-    const appendImportMap = (map: ImportMap): ImportMap => map;
-    return {appendImportMap};
-}
-
-export {domHandlerMock}
