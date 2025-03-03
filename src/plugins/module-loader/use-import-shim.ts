8840467cd57eb191b1c39b8ec2e619083e56ccf7
Aukevanoost <auke.v.o@hotmail.com>
2025-03-03 10:51:15 +0100

    chore(host-remote-entry) Added constants for namespaces and added feature to add host remoteEntry.json as manifest

--- src/plugins/module-loader/use-import-shim.ts
+++ src/plugins/module-loader/use-import-shim.ts
@@ -2,8 +2,8 @@ import type { ModuleLoaderConfig } from "../../lib/config/config.contract"
 
 declare function importShim<T>(url: string): T;
 
-const useImportMapShim = (): ModuleLoaderConfig => ({
-    importMapType: "importmap",
+const useImportMapShim = (cfg: {shimMode: boolean} = {shimMode: false}): ModuleLoaderConfig => ({
+    importMapType: cfg.shimMode ? "importmap-shim" : "importmap",
     loadModuleFn: (url) => importShim(url)
 })
 
--- src/plugins/storage/local-storage.ts
+++ src/plugins/storage/local-storage.ts
@@ -1,10 +1,11 @@
-import { type StorageEntryCreator, nfNamespace, type NfCache, type StorageEntry } from "./../../lib/handlers/storage/storage.contract";
+import { type StorageEntryCreator, type NfCache, type StorageEntry } from "./../../lib/handlers/storage/storage.contract";
+import {NF_STORAGE_ENTRY} from "../../lib/config/namespace.contract";
 
 const localStorageEntry: StorageEntryCreator = <TCache extends NfCache, K extends keyof TCache>(key: K, initialValue: TCache[K]) => {
 
     const entry: StorageEntry<TCache[K]> = {
         get(): TCache[K] {
-            const fromCache = localStorage.getItem(`${nfNamespace}.${String(key)}`);
+            const fromCache = localStorage.getItem(`${NF_STORAGE_ENTRY}.${String(key)}`);
             if (!fromCache) { 
                 entry.set(initialValue);
                 return initialValue;
@@ -12,7 +13,7 @@ const localStorageEntry: StorageEntryCreator = <TCache extends NfCache, K extend
             return JSON.parse(fromCache);
         },
         set(value: TCache[K]): StorageEntry<TCache[K]> {
-            localStorage.setItem(`${nfNamespace}.${String(key)}`, JSON.stringify(value));
+            localStorage.setItem(`${NF_STORAGE_ENTRY}.${String(key)}`, JSON.stringify(value));
             return entry;
         },
     };
