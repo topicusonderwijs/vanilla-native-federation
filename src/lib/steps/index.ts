d3e680c1f385bf6b37f11a7f97c4b5be168af11b
Aukevanoost <auke.v.o@hotmail.com>
2025-03-15 06:54:20 +0100

    fix(export) Exported interfaces for different steps

--- src/lib/steps/index.ts
+++ src/lib/steps/index.ts
@@ -1,3 +1,6 @@
+export { FetchRemoteEntries } from './1-fetch-remote-entries';
+export { CreateImportMap } from './2-create-import-map';
+export { ExposeModuleLoader, LoadRemoteModule } from './3-expose-module-loader';
 
 export * from './steps.contract';
 
