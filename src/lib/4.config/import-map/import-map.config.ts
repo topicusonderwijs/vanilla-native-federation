6dfed3b68edd7c90a32099fcc767d636cfde2435
Aukevanoost <auke.v.o@hotmail.com>
2025-05-10 11:29:28 +0200

    fix(iss-3): Fixed unintended override of full config object by configMappers

--- src/lib/4.config/import-map/import-map.config.ts
+++ src/lib/4.config/import-map/import-map.config.ts
@@ -1,7 +1,10 @@
 import type { ImportMapConfig, ImportMapOptions } from "lib/2.app/config/import-map.contract";
 import { useDefaultImportMap } from "./use-default";
 
-export const createImportMapConfig = (override: Partial<ImportMapOptions>): ImportMapConfig => ({
-    ...useDefaultImportMap(),
-    ...override
-});
+export const createImportMapConfig = (o: Partial<ImportMapOptions>): ImportMapConfig => {
+    const fallback = useDefaultImportMap();
+    return {
+        importMapType: o.importMapType ?? fallback.importMapType,
+        loadModuleFn: o.loadModuleFn ?? fallback.loadModuleFn
+    }
+};
